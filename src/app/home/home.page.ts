8226af90c3ef11c887ca6181fa2d066e64275192
ignacioc <ig.correar@duocuc.cl>
2024-08-21 12:41:16 -0400

    Maquetado basico

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,4 +9,23 @@ export class HomePage {
 
   constructor() {}
 
+  user = {
+    "username": "",
+    "password": ""
+  }
+
+  validar(){
+    if(this.user.username.length != 0) {
+      if(this.user.password.length != 0){
+        // Funciona
+        console.log("Usuario: "+this.user.username+"\nPassword: "+this.user.password)
+      }else{
+        // Contraseña vacia
+        console.log("Contraseña vacia")
+      }
+    }else{
+      // Usuario vacio
+        console.log("Usuario vacio")
+    }
+  }
 }
